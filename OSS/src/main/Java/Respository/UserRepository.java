38336fbacda622dee67c9df1cf65ea908f549253
CanhDuc710 <canhvdhe170639@fpt.edu.vn>
2024-06-16 08:47:02 +0700

    ko hieu gi het

---
+++ OSS/src/main/Java/Respository/UserRepository.java
@@ -0,0 +1,5 @@
+package Respository;
+import com.example.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface UserRepository extends JpaRepository<User, Long> {
+}
